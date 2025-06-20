d4e21946d5634f93fa3c2a01896d859a06286597
Gabriel Lima Dantas <101371170+PeDeLaranjaLima@users.noreply.github.com>
2025-06-20 19:30:29 -0300

    Versão 3 do projeto
    Adições:
    
    - Função para matricular o aluno em uma classe;
    - Função para pesquisar se uma classe existe;
    - Função para exibir a lista de classes existentes.

--- Sistema SIGMA - Atividade de ED.c
+++ Sistema SIGMA - Atividade de ED.c
@@ -25,8 +25,7 @@ O programa deve ter as seguintes funcionalidades.
     • Matricular aluno em determinada classe
     • Remover aluno de classe
     • Exibir listagem de alunos matriculados em uma classe
-    • Exibir lista de classes (com quantitativo de alunos matriculados em cada uma
-    delas e nome do professor)
+    • Exibir lista de classes (com quantitativo de alunos matriculados em cada uma delas e nome do professor)
     • Remover classe (só é possível se não tiver alunos matriculados nela)
     • Pesquisar e exibir qual a classe de um aluno
     Obs: Com relação às informações das classes as séries podem ser 1o, 2o e etc. As turmas
@@ -117,18 +116,85 @@ int cadastrarClasse(tipoListaClasses *listaClasses, char serie[5], char turma, c
     }
 }
 
+/* Função para cadastrar novo aluno na classe*/
+int matricularAluno(tipoClasse *listaEnc, const char *nome, int *idade, char const *matricula) {
+    tipoAluno *novoAluno = (tipoAluno*) malloc (sizeof(tipoAluno)); 
+        if(novoAluno == NULL){
+            printf("Erro na alocacao de memoria!");
+            return 0;
+        }
+
+    strcpy(novoAluno->nome, nome);
+    novoAluno->idade = idade;
+    strcpy(novoAluno->matricula, matricula);
+    novoAluno->prox = NULL;
+
+    listaEnc->fim->prox = novoAluno;
+    listaEnc->quantAlunos++;
+
+    return 1;
+
+}
+
+/*Função que faz pesquisa sequencial para achar uma classe */
+tipoClasse* pesqClasse(tipoListaClasses *listaEnc, const char *turma, const char *serie, const char *etapa){
+    tipoClasse *atual;  //Variável que será usada para percorrer a lista*/
+    if(listaEnc->fim->prox == NULL)
+        return 0;
+    atual = listaEnc->fim->prox;    
+    while(atual != listaEnc->fim){
+        if((strcmp(turma, atual->serie) == 0) && (strcmp(serie, atual->turma) == 0) && (strcmp(etapa, atual->etapa) == 0)){                                                                                                
+            return atual;
+        } 
+        atual = atual->prox;
+    }
+    printf("Elemento não encontrado");
+    return NULL;
+}
+
+/* Função para exibir lista de classes (com quantitativo de alunos matriculados em cada uma delas e nome do professor) */
+int exibeClasses(tipoListaClasses *listaEnc){
+  tipoClasse *atual;  //Variável que será usada para percorrer a lista*/
+    if(listaEnc->fim->prox == NULL)
+        printf("\nNenhuma classe cadastrada.");
+        return 0;
+    atual = listaEnc->fim->prox;
+    printf("\nLista de calsses: ");
+    printf("\n- Série: %s\n- Turma: %s\n- Etapa: %s\n- Quantidade de alunos: %s\n- Nome do professor: %s",
+                                                        atual->serie, atual->turma, atual->etapa, atual->quantAlunos, atual->nomeProf);
+    while(atual != listaEnc->fim){
+        atual = atual->prox;
+        printf("\n- Série: %s\n- Turma: %s\n- Etapa: %s\n- Quantidade de alunos: %s\n- Nome do professor: %s",
+                                                        atual->serie, atual->turma, atual->etapa, atual->quantAlunos, atual->nomeProf);
+        }
+        return 1;
+}
+
 int main() {
 
     tipoListaClasses listaClasses; // Criação da lista que será passada nas funções de manipulação de nós cabeça com as classes das matérias
     inicializaListaClasses(&listaClasses); // Alocando o espaço na memória para essa lista
     tipoClasse classe; // Criação da lista que contém as informações dos alunos
+    tipoClasse *aux;
 
     int op; // Variável para controle do switch
+
+    /* Variáveis para pesquisar as classes - servem para auxiliar no cadastro do aluno */
+    char serie[5];
+    char turma;
+    char etapa[20];
+
+    /* Variáveis para cadastro do aluno */
+    char nome[50];
+    int idade;
+    char matricula[30];
 
     do {
         printf("\n---- Bem vindo ao menu inicial do SIGMA ----");
         printf("\n\n        Menu inical");
         printf("\n 1 - Cadastrar nova classe no sistema");
+        printf("\n 2 - Cadastrar novo aluno no sistema");
+        printf("\n 3 - Exibir todas as classes cadastradas no sistema");
         printf("\n 0 - Encerar o SIGMA");
 
         printf("\nDigite sua opção:");
@@ -153,7 +219,33 @@ int main() {
                     printf("Serie: %s\nTurma: %c\nEtapa: %s\nNome do professor: %s\nQuantidade de classes: %d", listaClasses.inicio->serie, listaClasses.inicio->turma, listaClasses.inicio->etapa, listaClasses.inicio->nomeProf, listaClasses.quant);
                 }
             break;
+            case 2: printf("\nSelecione a turma que deseja cadatrar o aluno");
+                printf("\n\nDigite a série");
+                scanf("%s", serie);
+                printf("\n\nDigite a turma");
+                scanf("%s", turma);
+                printf("\n\nDigite a etapa");
+                scanf("%s", etapa);
 
+                aux = pesqClasse(&listaClasses, serie, turma, etapa);
+
+                if (aux != NULL){
+                    printf("\nDigite o nome do aluno");
+                    scanf("%s", nome);
+                    printf("\nDigite a idade do aluno");
+                    scanf("%d", &idade);
+                    printf("\nDigite a matricula do aluno");
+                    scanf("%s", matricula);
+
+                    matricularAluno(&aux, nome, idade, matricula);
+
+                }
+                else 
+                    printf("\n\nALuno não cadastrado, tente novamente em uma turma existente");
+            break;
+            case 3:
+                exibeClasses(&listaClasses);
+            break;
     } // Fim do switch
 
     } while (op != 0); // Fim doa do-while
